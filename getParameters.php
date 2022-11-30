31bcacf1f46bac5f81e7d1b4f50a7f89d1e8f9b0
Zuzana Orinčáková <zuzana.orincakova@student.tuke.sk>
2022-11-30 19:15:02 +0100

    esp32 init

--- getParameters.php
+++ getParameters.php
@@ -1,13 +1,16 @@
 <?php
         echo "<h1>getParameters page</h1>";
         
-        $sn1 = $_GET["a"];
-        $sn2 = $_GET["b"];
+        $sn1 = $_GET["a"]; //nacitame jednu hodntu
+        $sn2 = $_GET["b"]; //druhu
         
         $text = "a=" . $sn1 . " b=" . $sn2;
-        $sum = $sn1 + $sn2;
+        $sum = $sn1 + $sn2; //zratame
         
         echo $text;
         echo "<br>";
-        echo "Sum:" . $sum;
+        echo "Sum:" . $sum; //vypiseme na web
+         // za urlku mojej stranky lomitko a getParameters.php za php ? a=10 & b=2
     ?>
+
+   
